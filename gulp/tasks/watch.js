57f9001a24f0a8ac928b09c68450b926d285d68b
ran <ran.apphill@gmail.com>
2017-05-26 19:46:01 +0300

    Took his files again

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,29 +1,27 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    browserSync = require('browser-sync').create();
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
+gulp.task('watch', function() {
 
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
 
-gulp.task('watch', function () {
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
 
-    browserSync.init({
-        server: {
-            baseDir: "app"
-        }
-    });
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
 
-    watch('./app/index.html', function () {
-        browserSync.reload();
-    });
-
-    watch('./app/assets/**/*.css', function () {
-        gulp.start('cssInject');
-    });
 });
 
-// css inject
-gulp.task('cssInject', ['styles'], function () {
-
-    return gulp.src('./app/temp/styles/styles.css')
-        .pipe(browserSync.stream());
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
